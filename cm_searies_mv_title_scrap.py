5b32faf22f2315c51807d891eb785ecd4e966727
dr.xpad <dr.xpad@gmail.com>
2020-12-19 16:16:24 +0630

    Add files via upload

---
+++ cm_searies_mv_title_scrap.py
@@ -0,0 +1,30 @@
+import time
+import sys
+import os
+import requests
+from bs4 import BeautifulSoup 
+import re
+print('===========================================')
+print('=                                         =')
+print('= "Channel Myanmer" Series Movies Titles  =')
+print('=                                         =')
+print('===========================================')
+print
+test = "Web Scraping Testing by Pyae Phyo Naung"
+
+for i in test:
+    time.sleep(0.03)
+    sys.stdout.write(i)
+print 
+for pg in range(1,27):
+    url = 'https://channelmyanmar.org/tvshows/page/{}/'.format(pg)
+    r = requests.get(url)
+    
+    print('===========================================')
+    print('                Page:'+str(pg)+'           ')
+    print('===========================================')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    title = soup.find_all("div", {"class":"item"})
+    for i in title:
+        title = i.find('h2').get_text()
+        print(title)
